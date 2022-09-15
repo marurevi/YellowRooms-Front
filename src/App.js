ec420c8548160117110ef9964c9953def27eb18c
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-09-15 12:36:39 -0300

    Create Reat App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Yellow Room</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
