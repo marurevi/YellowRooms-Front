be39e4173d6c29ed34fc1f9b3ff9f0f56c2b8b9d
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-09-15 15:42:21 -0300

    nav bar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Yellow Room</h1>
+        <Header />
       </header>
     </div>
   );
