787c0ace7f90dbd15a66dbe5450a65ce4eaa2186
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-15 22:41:53 +0300

    Create: empity navbar components

---
+++ src/components/AddRoom/AddRoom.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddRoom() {
+  return (
+    <div>AddRoom</div>
+  );
+}
---
+++ src/components/DeleteRoom/DeleteRoom.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DeleteRoom() {
+  return (
+    <div>DeleteRoom</div>
+  )
+}
