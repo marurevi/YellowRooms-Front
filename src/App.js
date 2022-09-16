8fc413e320f1d8e9c860f5907f1b3c005c1eb5e7
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-17 00:23:23 +0300

    Update: routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Splash from './components/Splash/Splash';
-import Rooms from './components/Rooms/Rooms';
-import AddRoom from './components/AddRoom/AddRoom';
-import DeleteRoom from './components/DeleteRoom/DeleteRoom';
-import Reservations from './components/Reservations/Reservations';
-import Reserve from './components/Reserve/Reserve';
-import SignUp from './components/SignUp/SignUp';
-import Login from './components/Login/Login';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Splash from "./components/Splash/Splash";
+import Rooms from "./components/Rooms/Rooms";
+import AddRoom from "./components/AddRoom/AddRoom";
+import DeleteRoom from "./components/DeleteRoom/DeleteRoom";
+import Reservations from "./components/Reservations/Reservations";
+import Reserve from "./components/Reserve/Reserve";
+import SignUp from "./components/SignUp/SignUp";
+import Login from "./components/Login/Login";
+import Detail from "./components/Detail/Detail";
 const App = () => (
   <div className="App">
     <BrowserRouter>
@@ -19,7 +19,8 @@ const App = () => (
         <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/Rooms/" element={<Rooms />} />
+        <Route path="/Rooms/*" element={<Rooms />} />
+        <Route path="/Rooms/:id" element={<Detail />} />
         <Route path="/AddRoom" element={<AddRoom />} />
         <Route path="/DeleteRoom" element={<DeleteRoom />} />
         <Route path="/Reservations" element={<Reservations />} />
--- src/components/Rooms/Rooms.js
+++ src/components/Rooms/Rooms.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import { NavLink, Routes, Route } from 'react-router-dom';
-import Detail from '../Detail/Detail';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 const Rooms = () => {
   const value = `/Rooms/id=${1}`;
   return (
     <>
       <h2>Rooms</h2>
-      <NavLink className="link" to={value}>Detail</NavLink>
-      <Routes>
-        <Route path=":id" element={<Detail />} />
-      </Routes>
+      <NavLink className="link" to={value}>
+        Detail
+      </NavLink>
     </>
   );
 };
