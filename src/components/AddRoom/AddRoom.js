ddbf28b00b944b0109bd3a6178a55070d3641448
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-16 22:22:00 +0300

    Add: new room intial form

--- src/components/AddRoom/AddRoom.js
+++ src/components/AddRoom/AddRoom.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const AddRoom = () => (
-  <div>AddRoom</div>
-);
+const AddRoom = () => {
+  return (
+    <form action="/">
+      <div>
+        <label>
+          Photo
+          <input type="text" name="photo" id="photo" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Name
+          <input type="text" name="name" id="name" />
+        </label>
+      </div>
+      <div>
+        {" "}
+        <label>
+          Stars
+          <input type="number" name="star" id="star" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Number people
+          <input type="number" name="no-allowed" id="no-allowed" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Description
+          <textarea name="description" id="description" />
+        </label>
+      </div>
+      <button type="submit">Add Room</button>
+    </form>
+  );
+};
 
 export default AddRoom;
