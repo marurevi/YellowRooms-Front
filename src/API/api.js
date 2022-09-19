c4677497e94a4a7b7abe704f271b51821399c284
Tadesse Alemayehu <tadalem26@gmail.com>
2022-09-19 23:08:11 +0300

    Update: api module and sendGet function

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,15 @@
 import axios from "axios";
-export const baseUrl = "http://localhost:3001/rooms";
-// get method, take url, if not use default baseURL
-export const sendGet = async ({ url = baseUrl } = {}) => {
-  return axios.get(url).then((response) => response.data);
+export const baseUrl = "http://localhost:3001/";
+
+// custom is to alternate the endpoint
+export const sendGet = async (custom = "") => {
+  console.log(baseUrl + custom);
+  return axios.get(baseUrl + custom).then((response) => response.data);
+};
+
+// TODO: send delete,
+export const sendDelete = async ({ url = baseUrl, id = "" } = {}) => {
+  return axios.delete(url + id).then((response) => {
+    console.log(response.status);
+  });
 };
--- src/Redux/Rooms/rooms.js
+++ src/Redux/Rooms/rooms.js
@@ -1,15 +1,25 @@
 import { sendGet } from "../../API/api";
 const GET_ROOMS = "details/rooms/GET_ROOMS";
-// const DELETE_ROOM = "details/rooms/DELETE_ROOM";
+const DELETE_ROOM = "details/rooms/DELETE_ROOM";
 export const getRoomsActionCreator = () => async (dispatch) => {
-  const rooms = await sendGet("this is anothereeeeee u  u  rlr");
+  const rooms = await sendGet("rooms");
   dispatch({ type: GET_ROOMS, payload: rooms });
+};
+
+// REVIEW: delete method
+export const deleteRoomActionCreator = (id) => async (dispatch) => {
+  const status = await sendDelete(`rooms/${id}`);
+  dispatch({ type: DELETE_ROOM, payload: { status, id } });
 };
 
 const roomsReducer = (state = [], action) => {
   switch (action.type) {
     case GET_ROOMS:
       return action.payload;
+    case DELETE_ROOM:
+      return action.payload.status
+        ? [...state].filter((room) => room.id !== action.payload.id)
+        : state;
     default:
       return state;
   }
