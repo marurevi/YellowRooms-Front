79de727ab7e119e87b1e063c569c47c051b84ab0
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-09-19 13:48:46 -0300

    create My Reservations page

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Reservation from './Reservation';
 
-const Reservations = () => (
-  <div>Reservations</div>
-);
+const Reservations = () => {
+  const [reservations, setReservations] = useState([]);
+  const reservationsURL = 'http://localhost:3001/reservations';
+
+  React.useEffect(() => {
+    axios.get(reservationsURL).then((response) => {
+      setReservations(response.data);
+    });
+  }, []);
+
+  if (!reservations[0]) return null;
+
+  return (
+    <div>
+      <h2>MY RESERVATIONS</h2>
+      {reservations.map((room) => (
+        <Reservation
+          key={room.Id}
+          roomId={room.room_id}
+          userId={room.user_id}
+          startDate={room.start_date}
+          endDate={room.end_date}
+          city={room.city}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Reservations;
