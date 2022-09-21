034c708cae1a0e726f355bcbae76253248ebee89
Tadesse Alemayehu <tadalem26@gmail.com>
2022-09-21 23:31:11 +0300

    Fix: a selected room to be reserved

--- src/components/Reserve/Reserve.js
+++ src/components/Reserve/Reserve.js
@@ -1,31 +1,36 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 const Reserve = () => {
-  const reservationsURL = 'http://localhost:3001/reservations';
-  const rooms = useSelector((state) => state.rooms);
+  let { room_id: roomId } = useParams();
+  roomId = roomId?.split("=")[1];
+  console.log(roomId);
+  const room = useSelector(
+    (state) => state.rooms.filter((room) => room.id === Number(roomId))[0]
+  );
+  const reservationsURL = "http://localhost:3001/reservations";
   const [form, setForm] = useState({
     user_id: 0,
-    room_id: 0,
-    firstName: '',
-    start_date: '',
-    end_date: '',
-    city: '',
+    firstName: "",
+    start_date: "",
+    end_date: "",
+    city: "",
   });
 
   const reserveRoomSubmit = async (e) => {
     e.preventDefault();
-    axios.post(reservationsURL, {
-      user_id: parseInt(form.user_id, 10),
-      room_id: parseInt(form.room_id, 10),
-      start_date: form.start_date,
-      end_date: form.end_date,
-      city: form.city,
-    })
+    axios
+      .post(reservationsURL, {
+        user_id: parseInt(form.user_id, 10),
+        room_id: parseInt(form.room_id, 10),
+        start_date: form.start_date,
+        end_date: form.end_date,
+        city: form.city,
+      })
       .then((response) => {
         if (response.status === 201) {
-          window.location.href = '/Reservations';
+          window.location.href = "/Reservations";
         }
       });
   };
@@ -37,23 +42,53 @@ const Reserve = () => {
     }));
   };
 
-  return (
+  return room ? (
     <div>
       <h2>Reserve Room</h2>
       <form onSubmit={reserveRoomSubmit}>
-        <input onChange={onChange} name="user_id" className="signInput" type="number" placeholder="User" required />
-        <select onChange={onChange} name="room_id" required>
-          <option>SELECT OPTION</option>
-          {rooms.map((room) => (
-            <option key={room.id} value={room.id}>{room.name}</option>
-          ))}
-        </select>
-        <input onChange={onChange} name="start_date" className="signInput" type="text" placeholder="Start reservation" required />
-        <input onChange={onChange} name="end_date" className="signInput" type="text" placeholder="End reservation" required />
-        <input onChange={onChange} name="city" className="signInput" type="text" placeholder="City" required />
-        <button disabled={form.room_id === 0 || form.room_id === 'SELECT OPTION'} type="submit">Create Reservation</button>
+        <input
+          onChange={onChange}
+          name="user_id"
+          className="signInput"
+          type="number"
+          placeholder="User"
+          required
+        />
+        <input type="text" name="room_id" disabled value={room.name} />
+        <input
+          onChange={onChange}
+          name="start_date"
+          className="signInput"
+          type="text"
+          placeholder="Start reservation"
+          required
+        />
+        <input
+          onChange={onChange}
+          name="end_date"
+          className="signInput"
+          type="text"
+          placeholder="End reservation"
+          required
+        />
+        <input
+          onChange={onChange}
+          name="city"
+          className="signInput"
+          type="text"
+          placeholder="City"
+          required
+        />
+        <button
+          disabled={form.room_id === 0 || form.room_id === "SELECT OPTION"}
+          type="submit"
+        >
+          Create Reservation
+        </button>
       </form>
     </div>
+  ) : (
+    <div> No room is set to reserve</div>
   );
 };
 
