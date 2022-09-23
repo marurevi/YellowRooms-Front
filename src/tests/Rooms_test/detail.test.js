c5970159eaf270ba0aebbbb2de0f508bba46e9c0
Tadesse Alemayehu <tadalem26@gmail.com>
2022-09-23 23:03:46 +0300

    Add: snapshot test for the Rooms, and Detail component

---
+++ src/tests/Rooms_test/detail.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import wrapper from "../wrapMeInValidComponents";
+import Detail from "../../components/Detail/Detail";
+
+test("Rooms show render the same", () => {
+  const jsonRep = renderer.create(wrapper(<Detail />)).toJSON();
+  expect(jsonRep).toMatchSnapshot();
+});
