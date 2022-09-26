d8b43b6a25e9cd410a683e24173cdb1a8dac132f
Tadesse Alemayehu <tadalem26@gmail.com>
2022-09-26 20:05:32 +0300

    Update: reducer name for user reducer

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,17 +2,17 @@ import {
   legacy_createStore as createStore,
   combineReducers,
   applyMiddleware,
-} from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import roomsReducer from './Rooms/rooms';
-import reservationsReducer from './Reservations/Reservations';
-import handleUser from './Authenticate/authentication';
+} from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import roomsReducer from "./Rooms/rooms";
+import reservationsReducer from "./Reservations/Reservations";
+import userReducer from "./Authenticate/authentication";
 
 const reducers = combineReducers({
   rooms: roomsReducer,
   reservations: reservationsReducer,
-  user: handleUser,
+  user: userReducer,
 });
 
 const store = createStore(reducers, applyMiddleware(thunk, logger));
