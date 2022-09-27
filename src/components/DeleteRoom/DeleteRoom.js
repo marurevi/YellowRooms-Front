d0df95b3cff4c9c732dac71220929c9bde508502
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-09-27 18:19:54 -0300

    basic style for delete page

--- src/components/DeleteRoom/DeleteRoom.js
+++ src/components/DeleteRoom/DeleteRoom.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import DeleteButton from '../partials/DeleteRoom';
+import './DeleteRoom.css';
 
 const DeleteRoom = () => {
   const rooms = useSelector((store) => store.rooms.rooms);
   return rooms.length > 0 ? (
-    <div>
+    <div className="deleteRoomPage">
       {rooms.map((room) => (
-        <div key={room.id}>
-          <p>{room.attributes.name}</p>
-          <div>
+        <div key={room.id} className="deleteDiv">
+          <img src={room.attributes.photo} alt="Room" className="deleteRoomPicture" />
+          <div className="deleteSection">
+            <p>{room.attributes.name}</p>
             <DeleteButton id={room.id} />
           </div>
         </div>
