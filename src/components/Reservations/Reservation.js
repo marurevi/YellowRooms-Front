e6af90c950475096c9c9f157f6772c17f61f856b
Tadesse Alemayehu <tadalem26@gmail.com>
2022-09-28 23:51:40 +0300

    Style: reservations

--- src/components/Reservations/Reservation.js
+++ src/components/Reservations/Reservation.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
-import DeleteReservation from '../partials/DeleteReservation';
+import React from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import DeleteReservation from "../partials/DeleteReservation";
 
 const Reservation = ({ reservationId }) => {
   console.log(reservationId);
-  const reservation = useSelector((store) => store.reservations.find(
-    (reservation) => reservation.id === reservationId,
-  ));
+  const reservation = useSelector((store) =>
+    store.reservations.find((reservation) => reservation.id === reservationId)
+  );
+  const reservedRoom = useSelector((store) =>
+    store.rooms.rooms.find(
+      (room) => Number(room.id) === reservation.attributes.room_id
+    )
+  );
+  console.log(reservedRoom);
   return (
-    <div className="reservation">
-      <p>{`Reservation city: ${reservation.attributes.city}`}</p>
-      <p>{`Reservation start date: ${reservation.attributes.start_date}`}</p>
-      <p>{`Reservation end date: ${reservation.attributes.end_date}`}</p>
-      <DeleteReservation id={reservation.id} />
+    <div
+      className="reservation"
+      style={{ backgroundImage: `url(${reservedRoom.attributes.photo})` }}
+    >
+      <div className="container">
+        <p>
+          <span className="label">Reserved Room Name: </span>
+          {reservedRoom.attributes.name}
+        </p>
+        <p>
+          <span className="label">Reservation city: </span>
+          {reservation.attributes.city}
+        </p>
+        <p>
+          <span className="label">Reservation start date: </span>
+          {reservation.attributes.start_date}
+        </p>
+        <p>
+          <span className="label">Reservation end date: </span>
+          {reservation.attributes.end_date}
+        </p>
+        <DeleteReservation id={reservation.id} />
+      </div>
     </div>
   );
 };
